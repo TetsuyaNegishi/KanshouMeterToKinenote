a25db754a4f7a5da2195b6d3d269e2b50765da55
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-09-24 19:09:45 +0900

    update:実行時にブラウザ非表示、関数名削除、ログイン後ユーザ名が表示するまで待機

--- src/KanshouMeter/fetchCookies.js
+++ src/KanshouMeter/fetchCookies.js
@@ -3,14 +3,14 @@ import { KANSHOU_METER_URL } from './constants';
 
 require('dotenv').config();
 
-export default function getCookies() {
-  const nightmare = Nightmare({ show: true });
+export default function () {
+  const nightmare = Nightmare();
   return new Promise((resolve, reject) => {
     nightmare.goto(`${KANSHOU_METER_URL}/login`)
       .type('input[name="mail"]', process.env.KANSHOU_METER_EMAIL_ADDRESS)
       .type('input[name="password"]', process.env.KANSHOU_METER_PASSWORD)
       .click('input[value=" ロ グ イ ン "]')
-      .wait(4000)
+      .wait('#navi_top_link_2')
       .cookies.get()
       .end()
       .then((cookies) => {
