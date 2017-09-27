22cf5dc32b518bf71016c2ee4ef93f0bc89c17e7
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-09-27 21:12:37 +0900

    feat:映画のデータをファイルに出力

--- src/importData.js
+++ src/importData.js
@@ -2,12 +2,16 @@ import fetchCookies from './KanshouMeter/fetchCookies';
 import fetchMoviesUrl from './KanshouMeter/fetchMoviesUrl';
 import fetchMoviesDataList from './KanshouMeter/fetchMoviesDataList';
 
+const fs = require('fs');
+
 async function exec() {
   const cookies = await fetchCookies();
   const Urls = await fetchMoviesUrl(cookies);
   console.log(Urls);
   const data = await fetchMoviesDataList(cookies, Urls);
   console.log(data);
+
+  fs.writeFileSync('moviesData.json', JSON.stringify(data, null, '    '));
 }
 
 exec().then(() => {
